abdad014600365d901a9c871499413c3e9e7f05f
Robert Smidt <rsmidt909@hotmail.com>
2019-04-23 13:52:19 -0500

    removed all console.log commands and replaced with getEllemtnBYId. placed text inside of respecive boxes on html side.

--- diceGame.js
+++ diceGame.js
@@ -16,12 +16,13 @@ let dragonBaseHealth = 5;
 
 
 
-/*let playerBox = document.getElementById("playerBox");
+let playerBox = document.getElementById("playerBox");
 let ctx = playerBox.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 750, 500);
-ctx.globalAlpha = 0.1;
-*/
+ctx.fillText('Player Attack',10,450);
+ctx.font = sans-serif;
+ctx.fillText('Player Health',15,150);
+
+
 
 
 
@@ -44,6 +45,7 @@ return roll;
 
 function playerHealthNA (){
     let totalPlayerHealth = pickPlayerHealth()*playerBaseHealth
+    
     return totalPlayerHealth;
 }
 
@@ -59,6 +61,7 @@ function playerAttackOWep (){
 
 function playerAttackTotal (playerAttack){
     let totalPlayerAttack = pickPlayerAttack()*playerAttack
+    
     return totalPlayerAttack;
 }
 
@@ -88,12 +91,15 @@ function playerAttackCheck(playerAttack,dragonHealth){
         return console.log('You didnt beat the Dragon, and he ate you!!!')}
 }
 
+
 function masterFunc(){
+    
     if (gameContinue ===true){
        let playerHealth = playerHealthNA(playerBaseHealth);
        playerHealth = playerHealthTotal(playerHealth);
        let playerAttack = playerAttackOWep();
        playerAttack = playerAttackTotal(playerAttack);
+    
        //maybe affinity here
        //maybe type of dragon here
        let dragonHealth = dragonHealthTotal(dragonBaseHealth);
@@ -103,34 +109,35 @@ function masterFunc(){
            return;
        } else{ 
        playerAttackCheck(playerAttack,dragonHealth,gameContinue);
-}}};
+}}           gameContinue = true;
+};
 
 
 function pickWeapon (){
     let weapon;
      switch(diceRoll(6)) {
          case 1:
-         console.log('Toilet plunger + 1')
+         document.getElementById("wep").innerHTML ='Toilet plunger + 1';
          weapon = 1;
          break;      
          case 2:
-         console.log('Wooden Sword + 2')
+         document.getElementById("wep").innerHTML ='Wooden Sword + 2';
          weapon = 2;
          break;
          case 3:
-         console.log('Aluminum Bat + 3')
+         document.getElementById("wep").innerHTML = 'Aluminum Bat + 3';
          weapon = 3;
          break;
          case 4:
-         console.log('Short Sword + 4')
+         document.getElementById("wep").innerHTML ='Short Sword + 4';
          weapon = 4;
          break;
          case 5:
-         console.log('Flail + 5')
+         document.getElementById("wep").innerHTML ='Flail + 5';
          weapon = 5;
          break;
          case 6:
-         console.log('Excalibur + 6')
+         document.getElementById("wep").innerHTML ='Excalibur + 6';
          weapon = 6;
          break;
          default: weapon = 0;
@@ -144,19 +151,19 @@ function pickArmor(){
 let armor;
     switch(diceRoll(4)){
         case 1:
-        console.log('Hide armor + 1')
+        document.getElementById("armor").innerHTML='Hide armor + 1';
         armor = 1;
         break; 
         case 2:
-        console.log('Plate Armor + 2')
+        document.getElementById("armor").innerHTML='Plate Armor + 2';
         armor = 2;
         break;
         case 3:
-        console.log('Mitheral Armor + 3')
+        document.getElementById("armor").innerHTML='Mitheral Armor + 3';
         armor = 3;
         break;
         case 4:
-        console.log('King Gilgamesh\'s Armor + 4')
+        document.getElementById("armor").innerHTML='King Gilgamesh\'s Armor + 4';
         armor = 4;
         break;
         default: armor = 0;
@@ -212,43 +219,43 @@ function pickPlayerHealth(){
  let playerHealth;
     switch(diceRoll(10)){
         case 1:
-        console.log('Frogs have better health than you + 1')
+        document.getElementById("pHealth").innerHTML ='Frogs have better health than you + 1';
         playerHealth = 1;
         break;
         case 2:
-        console.log('Youre just a bean pole + 2')
+        document.getElementById("pHealth").innerHTML ='Youre just a bean pole + 2';
         playerHealth = 2;
         break;
         case 3:
-        console.log('Lord Farquad like health + 3')
+        document.getElementById("pHealth").innerHTML ='Lord Farquad like health + 3';
         playerHealth = 3;
         break;
         case 4:
-        console.log('Youve seen an apple BEFORE type health + 4')
+        document.getElementById("pHealth").innerHTML ='Youve seen an apple BEFORE type health + 4';
         playerHealth = 4;
         break;
         case 5:
-        console.log('You eat an orange a day. + 5')
+        document.getElementById("pHealth").innerHTML ='You eat an orange a day. + 5';
         playerHealth = 5;
         break;
         case 6:
-        console.log('You eat your Wheaties before you eat your Wheaties. + 6')
+        document.getElementById("pHealth").innerHTML ='You eat your Wheaties before you eat your Wheaties. + 6';
         playerHealth = 6;
         break;
         case 7:
-        console.log('People try not to make eye contact from your masculanity + 7')
+        document.getElementById("pHealth").innerHTML ='People try not to make eye contact from your masculanity + 7';
         playerHealth = 7;
         break;
         case 8:
-        console.log('Just a gym rat + 8')
+        document.getElementById("pHealth").innerHTML ='Just a gym rat + 8';
         playerHealth = 8;
         break;
         case 9:
-        console.log('Youre on par with Superman + 9')
+        document.getElementById("pHealth").innerHTML ='Youre on par with Superman + 9';
         playerHealth = 9;
         break;
         case 10:
-        console.log('GOD-LIKE + 10')
+        document.getElementById("pHealth").innerHTML ='GOD-LIKE + 10';
         playerHealth = 10;
         break;
         default: playerHealth = 0;
@@ -321,83 +328,83 @@ function pickPlayerAttack(){
     let playerAttack;
     switch(diceRoll(20)){
         case 1:
-        console.log('You forgot how to swing + 1')
+        document.getElementById("playerAttack").innerHTML ='You forgot how to swing + 1';
         playerAttack = 1;
         break;
         case 2:
-        console.log('You somewhat remembered how to swing + 2')
+        document.getElementById("playerAttack").innerHTML ='You somewhat remembered how to swing + 2';
         playerAttack = 2;
         break;
         case 3:
-        console.log('You attempt to swing + 3')
+        document.getElementById("playerAttack").innerHTML ='You attempt to swing + 3';
         playerAttack = 3;
         break;
         case 4:
-        console.log('You do a thing that resembles a swing + 4')
+        document.getElementById("playerAttack").innerHTML ='You do a thing that resembles a swing + 4';
         playerAttack = 4;
         break;
         case 5:
-        console.log('You swing accidently + 5')
+        document.getElementById("playerAttack").innerHTML ='You swing accidently + 5';
         playerAttack = 5;
         break;
         case 6:
-        console.log('You swing like a baby + 6')
+        document.getElementById("playerAttack").innerHTML ='You swing like a baby + 6';
         playerAttack = 6;
         break;
         case 7:
-        console.log('You swing like an old lady + 7')
+        document.getElementById("playerAttack").innerHTML ='You swing like an old lady + 7';
         playerAttack = 7;
         break;
         case 8:
-        console.log('You swing for the first time in your life + 8')
+        document.getElementById("totalAttack").innerHTML ='You swing for the first time in your life + 8';
         playerAttack = 8;
         break;
         case 9:
-        console.log('You swing abnormally + 9')
+        document.getElementById("playerAttack").innerHTML ='You swing abnormally + 9';
         playerAttack = 9;
         break;
         case 10:
-        console.log('You swing normally + 10')
+        document.getElementById("playerAttack").innerHTML ='You swing normally + 10';
         playerAttack = 10;
         break;
         case 11:
-        console.log('You step forward and swing + 11')
+        document.getElementById("playerAttack").innerHTML ='You step forward and swing + 11';
         playerAttack = 11;
         break;
         case 12:
-        console.log('You lunge and swing + 12')
+        document.getElementById("playerAttack").innerHTML ='You lunge and swing + 12';
         playerAttack = 12;
         break;
         case 13:
-        console.log('You run and swing + 13')
+        document.getElementById("playerAttack").innerHTML ='You run and swing + 13';
         playerAttack = 13;
         break;
         case 14:
-        console.log('You run then jump and swing + 14')
+        document.getElementById("playerAttack").innerHTML ='You run then jump and swing + 14';
         playerAttack = 14;
         break;
         case 15:
-        console.log('You jump and do a circle swing + 15')
+        document.getElementById("playerAttack").innerHTML ='You jump and do a circle swing + 15';
         playerAttack = 15;
         break;
         case 16:
-        console.log('You swing like a champion + 16')
+        document.getElementById("playerAttack").innerHTML ='You swing like a champion + 16';
         playerAttack = 16;
         break;
         case 17:
-        console.log('You do a backflip and then swing + 17')
+        document.getElementById("playerAttack").innerHTML ='You do a backflip and then swing + 17';
         playerAttack = 17;
         break;
         case 18:
-        console.log('You swing like a Master Jedi + 18')
+        document.getElementById("playerAttack").innerHTML ='You swing like a Master Jedi + 18';
         playerAttack = 18;
         break;
         case 19:
-        console.log('You swing perfectly with your eyes closed + 19')
+        document.getElementById("playerAttack").innerHTML ='You swing perfectly with your eyes closed + 19';
         playerAttack = 19;
         break;
         case 20:
-        console.log('BANKAI + 20')
+        document.getElementById("playerAttack").innerHTML ='BANKAI + 20';
         playerAttack = 20;
         break;
         default: playerAttack = 0;
